bd67ac7ec89f8d3cb414db9d134f9c98d63a3951
Neto test <nastiadm1005@gmail.com>
2026-06-24 16:27:53 +0300

    Добавлен код в models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
